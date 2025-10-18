30e8f9dc1cbbdf53ace9ea672b669e76c3df0ed0
lameessa <444201177@student.ksu.edu.sa>
2025-10-18 17:24:17 +0300

    Database linking to main + log transaction page

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:supabase_flutter/supabase_flutter.dart';
 import 'screens/welcome_screen.dart';
 import 'screens/login_screen.dart';
 import 'screens/profile_main.dart';
@@ -10,10 +11,23 @@ import 'screens/profile_setup/setup_income_screen.dart';
 import 'screens/profile_setup/setup_expenses_screen.dart';
 import 'screens/profile_setup/setup_balance_screen.dart';
 import 'screens/Dashboard/dashboard_page.dart';
-
-
 import 'screens/Savings/savings_page.dart';
-void main() => runApp(const SurraApp());
+
+// Supabase credentials
+const supabaseUrl = 'https://xvnkuqqzlstzwgeecijn.supabase.co';
+const supabaseKey = String.fromEnvironment('SUPABASE_KEY');
+
+Future<void> main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+
+  // Initialize Supabase
+  await Supabase.initialize(
+    url: supabaseUrl,
+    anonKey: supabaseKey,
+  );
+
+  runApp(const SurraApp());
+}
 
 class SurraApp extends StatelessWidget {
   const SurraApp({super.key});
@@ -34,7 +48,7 @@ class SurraApp extends StatelessWidget {
         '/welcome': (context) => const WelcomeScreen(),
         '/login': (context) => const LoginScreen(),
         '/profile': (context) => const ProfileMainPage(),
-        '/dashboard': (context) => const DashboardPage(), 
+        '/dashboard': (context) => const DashboardPage(),
         '/signup': (context) => const SignUpScreen(),
         '/editProfile': (context) => const EditProfilePage(),
         '/savings': (context) => const SavingsPage(),
